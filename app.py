9f4e79e1ba1c1387924334ec1d56f98f35beae5e
akhil-ibm <121149449+akhil-ibm@users.noreply.github.com>
2022-12-21 16:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Hello all, welcome to my application."
+    return "Hello Everyone, welcome to my application."
  
 
 @app.route("/healthz")
